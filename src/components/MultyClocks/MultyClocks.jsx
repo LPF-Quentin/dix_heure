8e320390d84de922b94713f83069c1fffbc11914
LPF-Quentin <quentin-rousseau@orange.fr>
2023-08-08 11:59:27 +0200

    ajustement css

---
+++ src/components/MultyClocks/MultyClocks.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+const MultyClocks = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData(); // Appel initial pour récupérer l'heure actuelle
+    const interval = setInterval(fetchData, 1000); // Mettre à jour l'heure toutes les 1000 ms
+
+    return () => clearInterval(interval); // Nettoie l'intervalle lorsque le composant est démonté
+  }, []);
+
+  const fetchData = () => {
+    fetch("http://worldtimeapi.org/api/timezone/America/Caracas")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de l'appel API:", error);
+      });
+  };
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div id="multyClock">
+      <div id="multyContentClock">
+        {data.datetime}
+        <br />
+        {data.timezone}
+      </div>
+    </div>
+  );
+};
+
+export default MultyClocks;
--- src/pages/AllClocks/AllClocks.jsx
+++ src/pages/AllClocks/AllClocks.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import Navigation from "../../components/Navigation/Navigation";
-import MainTime from "../../components/MainTime/MainTime";
-import Logo from "../../components/Logo/Logo";
+// import MainTime from "../../components/MainTime/MainTime";
+// import Logo from "../../components/Logo/Logo";
+import MultyClocks from "../../components/MultyClocks/MultyClocks";
 
 const AllClocks = () => {
   return (
     <div>
       <Navigation />
       <h1>a propos</h1>
-        <div id="allClocks">
-          <MainTime />
-          <MainTime />
-          <MainTime />
-          <MainTime />
-          <MainTime />
-          <MainTime />
-        </div>
+      <div id="allClocks">
+        <MultyClocks />
+        <MultyClocks />
+        <MultyClocks />
+        <MultyClocks />
+        <MultyClocks />
+        <MultyClocks />
+      </div>
     </div>
   );
 };
